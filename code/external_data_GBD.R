64e2da1ce90932193faede8002155bc49d98201b
Anshu Uppal <anshu.uppal87@gmail.com>
2025-08-15 10:00:58 +0200

    split file to have API data in one, and GBD data in another

---
+++ code/external_data_GBD.R
@@ -0,0 +1,130 @@
+# --- GBD data ####
+# Global Burden of Disease Collaborative Network. Global Burden of Disease Study 2021 (GBD 2021) Results.
+# Seattle, United States: Institute for Health Metrics and Evaluation (IHME), 2022.
+# Available from <https://vizhub.healthdata.org/gbd-results/>.
+
+## All causes data ####
+# **Query:**  
+#   
+#   **GBD estimate:** Cause of death of injury  
+# **Measure:** Deaths; DALYs  
+# **Metric:** Rate  
+# **Cause:** All level 3 causes  
+# **Location:** All countries and territories  
+# **Age:** Age-standardized  
+# **Sex:** Both; Female; Male  
+# **Year:** Select all  
+# 
+# This query generated 14 CSV files that I combined and then saved as RDS format in order to save diskspace
+
+# Uncomment below code for first run
+# GBD <-
+#   # List the filenames of each CSV file in the folder
+#   list.files(path = here("data", "GBD"),
+#                   pattern = "\\.csv$",
+#                   full.names = TRUE) |>
+#   # Read them in using data.table::fread()
+#   map_df(~data.table::fread(.)) |>
+#   # Clean the names
+#   janitor::clean_names() |>
+#   # Format variables
+#   mutate(
+#     across(c(measure_name, location_name, sex_name,
+#                   cause_name, metric_name), as.factor),
+#     date = ymd(paste0(year, "-01-01"))
+#     )
+# 
+# # Split the file by measure
+# GBD_DALY <- GBD |> filter(measure_name == "DALYs (Disability-Adjusted Life Years)")
+# GBD_deaths <- GBD |> filter(measure_name == "Deaths")
+# 
+# # Save as RDS
+# saveRDS(GBD_DALY, file = here("data", "GBD", "GBD_DALY.rds"))
+# saveRDS(GBD_deaths, file = here("data", "GBD", "GBD_deaths.rds"))
+
+GBD_DALY <- readRDS(here("data", "GBD", "GBD_DALY.rds")) |> 
+  left_join(country_list, join_by(location_name == english_formal)) |> 
+  mutate(country = case_when(location_name == "Global" ~ "Global", 
+                             .default = country)) |> 
+  filter(!is.na(country))
+
+GBD_deaths <- readRDS(here("data", "GBD", "GBD_deaths.rds"))  |> 
+  left_join(country_list, join_by(location_name == english_formal)) |> 
+  mutate(country = case_when(location_name == "Global" ~ "Global", 
+                             .default = country)) |> 
+  filter(!is.na(country))
+
+## Maternal disorders - Level 4 causes ####
+# **Query:**  
+#   
+#   **GBD estimate:** Cause of death or injury  
+# **Measure:** Deaths; DALYs  
+# **Metric:** Rate  
+# **Cause:** Maternal disorders as well as all associated level 4 causes 
+# **Location:** All countries and territories  
+# **Age:** Age-standardized; 
+# **Sex:** Female  
+# **Year:** 2021 
+# 
+# This query generated a CSV file that I split by Deaths and DALYs and saved in RDS format in order to save diskspace.
+# maternal_disorders <-
+#   # List the filenames of each CSV file in the folder
+#   list.files(path = here("data", "GBD", "maternal_disorders"),
+#              pattern = "\\.csv$",
+#              full.names = TRUE) |>
+#   # Read them in using data.table::fread()
+#   map_df(~data.table::fread(.)) |>
+#   # Clean the names
+#   janitor::clean_names() |>
+#   # Format variables
+#   mutate(
+#     across(c(measure_name, location_name, sex_name,
+#              cause_name, metric_name), as.factor),
+#     date = ymd(paste0(year, "-01-01"))
+#   ) |> 
+#   mutate(age_name = fct_relevel(age_name, "Age-standardized")) |> 
+#   filter(!age_name %in% c("65-69 years", "60-64 years")) |> 
+#   droplevels()
+# 
+# # Split the file by measure
+# maternal_disorders_DALY <- maternal_disorders |> filter(measure_name == "DALYs (Disability-Adjusted Life Years)")
+# maternal_disorders_deaths <- maternal_disorders |> filter(measure_name == "Deaths")
+# 
+# # Save as RDS
+# saveRDS(maternal_disorders_DALY, file = here("data", "GBD", "maternal_disorders", "maternal_disorders_DALY.rds"))
+# saveRDS(maternal_disorders_deaths, file = here("data", "GBD", "maternal_disorders", "maternal_disorders_deaths.rds"))
+
+maternal_disorders_DALY <- readRDS(here("data", "GBD", "maternal_disorders", "maternal_disorders_DALY.rds")) |> 
+  left_join(country_list, join_by(location_name == english_formal)) |> 
+  mutate(country = case_when(location_name == "Global" ~ "Global", 
+                             .default = country)) |> 
+  filter(!is.na(country))
+
+maternal_disorders_deaths <- readRDS(here("data", "GBD", "maternal_disorders", "maternal_disorders_deaths.rds")) |> 
+  left_join(country_list, join_by(location_name == english_formal)) |> 
+  mutate(country = case_when(location_name == "Global" ~ "Global", 
+                             .default = country)) |> 
+  filter(!is.na(country))
+
+# **Maternal haemorrhage** includes both postpartum haemorrhage (defined as blood loss ≥500 ml for vaginal delivery and ≥1000 ml for caesarean delivery) and antepartum haemorrhage (defined as vaginal bleeding from any cause at or beyond 20 weeks of gestation).  
+# 
+# **Maternal sepsis** is defined as a temperature <36°C or >38°C and clinical signs of shock (systolic blood pressure <90 mmHg and tachycardia >120 bpm). **Other maternal infections** are defined as any maternal infections excluding HIV, STI, or not related to pregnancy. 
+# 
+# **Maternal hypertensive disorders** include gestational hypertension (onset after 20 weeks gestation), pre-eclampsia, severe preeclampsia, and eclampsia, but exclude chronic hypertension (onset prior to pregnancy or prior to 20 weeks gestation) unless superimposed preeclampsia or eclampsia develop.
+# 
+# **Maternal obstructed labour and uterine rupture** aggregates obstructed labour (arrest in the first or second stage of active labour despite sufficient contractions), uterine rupture (non-surgical breakdown of uterine wall), and fistula (an abnormal opening between the vagina and the bladder or rectum following childbirth). 
+# 
+# **Abortion** is defined as elective or medically indicated termination of pregnancy at any gestational age. **Miscarriage** is defined as spontaneous loss of pregnancy before 24 weeks of gestation with complications requiring medical care.
+# 
+# **Ectopic pregnancy** is defined as pregnancy occurring outside of the uterus.
+# 
+# **Indirect maternal deaths** are due to existing diseases that are exacerbated by pregnancy. Examples include maternal infections and parasitic diseases complicating pregnancy, childbirth, and the puerperium, and diabetes in pregnancy, childbirth, and the puerperium. 
+# 
+# **Late maternal deaths** are deaths that occur six weeks to one year after the end of pregnancy, excluding incidental deaths.
+# 
+# **Maternal deaths aggravated by HIV/AIDS** are deaths occurring in HIV-positive women whose pregnancy has exacerbated their HIV/AIDS, leading to death.
+# 
+# **Other direct maternal disorders** encompasses a wide range of maternal disorders that do not map to other diseases in the GBD cause list, including other fatal or non-fatal complications occurring during pregnancy, childbirth, and the postpartum period. 
+# 
+# See also Cresswell et al. for more background on causes of maternal deaths:  
+#   Cresswell, Jenny A, Monica Alexander, Michael Y C Chong, Heather M Link, Marija Pejchinovska, Ursula Gazeley, Sahar M A Ahmed, et al. “Global and Regional Causes of Maternal Deaths 2009–20: A WHO Systematic Analysis.” The Lancet Global Health 13, no. 4 (April 2025): e626–34. <https://doi.org/10.1016/S2214-109X(24)00560-6>.
--- code/external_data_OData.R
+++ code/external_data_OData.R
@@ -7,9 +7,8 @@ pacman::p_load(
   DT,
   sf,
   necountries,
-  patchwork,
-  Hmisc,
-  ggmapinset
+  patchwork
+  # Hmisc
 )
 
 # Load or install packages from GitHub:
@@ -138,13 +137,12 @@ WB_income_codes <- gho_api$path("Dimension", "WORLDBANKINCOMEGROUP", "DimensionV
   select(Code, Title) |> 
   rename(WORLDBANKINCOMEGROUP = Code, WB_income_group = Title)
 
-xxxcc <- gho_api$path("Dimension", "DHSMICSGEOREGION", "DimensionValues")$retrieve()$value
 
 
 ## Search GHO codes ####
 search_term <- "partum|natal"
 gho_indicators |> filter(str_detect(IndicatorName, regex(search_term, ignore_case = TRUE))|
-                      str_detect(IndicatorCode, regex(search_term, ignore_case = TRUE)))
+                           str_detect(IndicatorCode, regex(search_term, ignore_case = TRUE)))
 
 ## Indicators ####
 
@@ -167,7 +165,7 @@ UHC_all <- bind_rows(UHC_INDEX_REPORTED,
     WORLDBANKINCOMEGROUP = case_when(SpatialDimType %in% c("WORLDBANKINCOMEGROUP", "GLOBAL")~SpatialDim)
   ) |> 
   left_join(gho_indicators) |> 
-left_join(country_codes) |> 
+  left_join(country_codes) |> 
   left_join(region_codes) |> 
   left_join(UN_region_codes) |> 
   left_join(WB_income_codes) |> 
@@ -184,8 +182,8 @@ MMR <- gho_api$path("MDG_0000000026")$retrieve()$value |> tibble() |>
     REGION = case_when(SpatialDimType %in% c("REGION", "GLOBAL")~SpatialDim)
   ) |> 
   left_join(gho_indicators) |> 
-    left_join(country_codes) |> 
-    left_join(region_codes) |> 
+  left_join(country_codes) |> 
+  left_join(region_codes) |> 
   rename(YEAR = TimeDim) |> 
   mutate(
     # NumericValue = as.numeric(NumericValue),
@@ -204,8 +202,6 @@ MMR <- gho_api$path("MDG_0000000026")$retrieve()$value |> tibble() |>
       levels = c("<10","10-19", "20-99","100-299", "300-499", "500+" )
     )
   )
-
-aanh <- gho_api$path("UNICEF_PNCMOTHER")$retrieve()$value
 
 ## Health check after delivery ####
 postnatal_care <- gho_api$path("UNICEF_PNCMOTHER")$retrieve()$value |> tibble() |> 
@@ -444,133 +440,9 @@ HPV_coverage <- gho_api$path("SDGHPVRECEIVED")$retrieve()$value |> tibble() |>
     year = ymd(paste0(YEAR, "-01-01"))
   )
 
-# --- GBD data ####
-# Global Burden of Disease Collaborative Network. Global Burden of Disease Study 2021 (GBD 2021) Results.
-# Seattle, United States: Institute for Health Metrics and Evaluation (IHME), 2022.
-# Available from <https://vizhub.healthdata.org/gbd-results/>.
-
-## All causes data ####
-# **Query:**  
-#   
-#   **GBD estimate:** Cause of death of injury  
-# **Measure:** Deaths; DALYs  
-# **Metric:** Rate  
-# **Cause:** All level 3 causes  
-# **Location:** All countries and territories  
-# **Age:** Age-standardized  
-# **Sex:** Both; Female; Male  
-# **Year:** Select all  
-# 
-# This query generated 14 CSV files that I combined and then saved as RDS format in order to save diskspace
-
-# Uncomment below code for first run
-# GBD <-
-#   # List the filenames of each CSV file in the folder
-#   list.files(path = here("data", "GBD"),
-#                   pattern = "\\.csv$",
-#                   full.names = TRUE) |>
-#   # Read them in using data.table::fread()
-#   map_df(~data.table::fread(.)) |>
-#   # Clean the names
-#   janitor::clean_names() |>
-#   # Format variables
-#   mutate(
-#     across(c(measure_name, location_name, sex_name,
-#                   cause_name, metric_name), as.factor),
-#     date = ymd(paste0(year, "-01-01"))
-#     )
-# 
-# # Split the file by measure
-# GBD_DALY <- GBD |> filter(measure_name == "DALYs (Disability-Adjusted Life Years)")
-# GBD_deaths <- GBD |> filter(measure_name == "Deaths")
-# 
-# # Save as RDS
-# saveRDS(GBD_DALY, file = here("data", "GBD", "GBD_DALY.rds"))
-# saveRDS(GBD_deaths, file = here("data", "GBD", "GBD_deaths.rds"))
-
-GBD_DALY <- readRDS(here("data", "GBD", "GBD_DALY.rds")) |> 
-  left_join(country_list, join_by(location_name == english_formal)) |> 
-  mutate(country = case_when(location_name == "Global" ~ "Global", 
-                             .default = country)) |> 
-  filter(!is.na(country))
-
-GBD_deaths <- readRDS(here("data", "GBD", "GBD_deaths.rds"))  |> 
-  left_join(country_list, join_by(location_name == english_formal)) |> 
-  mutate(country = case_when(location_name == "Global" ~ "Global", 
-                             .default = country)) |> 
-  filter(!is.na(country))
-
-## Maternal disorders - Level 4 causes ####
-# **Query:**  
-#   
-#   **GBD estimate:** Cause of death or injury  
-# **Measure:** Deaths; DALYs  
-# **Metric:** Rate  
-# **Cause:** Maternal disorders as well as all associated level 4 causes 
-# **Location:** All countries and territories  
-# **Age:** Age-standardized; 
-# **Sex:** Female  
-# **Year:** 2021 
-# 
-# This query generated a CSV file that I split by Deaths and DALYs and saved in RDS format in order to save diskspace.
-# maternal_disorders <-
-#   # List the filenames of each CSV file in the folder
-#   list.files(path = here("data", "GBD", "maternal_disorders"),
-#              pattern = "\\.csv$",
-#              full.names = TRUE) |>
-#   # Read them in using data.table::fread()
-#   map_df(~data.table::fread(.)) |>
-#   # Clean the names
-#   janitor::clean_names() |>
-#   # Format variables
-#   mutate(
-#     across(c(measure_name, location_name, sex_name,
-#              cause_name, metric_name), as.factor),
-#     date = ymd(paste0(year, "-01-01"))
-#   ) |> 
-#   mutate(age_name = fct_relevel(age_name, "Age-standardized")) |> 
-#   filter(!age_name %in% c("65-69 years", "60-64 years")) |> 
-#   droplevels()
-# 
-# # Split the file by measure
-# maternal_disorders_DALY <- maternal_disorders |> filter(measure_name == "DALYs (Disability-Adjusted Life Years)")
-# maternal_disorders_deaths <- maternal_disorders |> filter(measure_name == "Deaths")
-# 
-# # Save as RDS
-# saveRDS(maternal_disorders_DALY, file = here("data", "GBD", "maternal_disorders", "maternal_disorders_DALY.rds"))
-# saveRDS(maternal_disorders_deaths, file = here("data", "GBD", "maternal_disorders", "maternal_disorders_deaths.rds"))
-
-maternal_disorders_DALY <- readRDS(here("data", "GBD", "maternal_disorders", "maternal_disorders_DALY.rds")) |> 
-  left_join(country_list, join_by(location_name == english_formal)) |> 
-  mutate(country = case_when(location_name == "Global" ~ "Global", 
-                             .default = country)) |> 
-  filter(!is.na(country))
-
-maternal_disorders_deaths <- readRDS(here("data", "GBD", "maternal_disorders", "maternal_disorders_deaths.rds")) |> 
-  left_join(country_list, join_by(location_name == english_formal)) |> 
-  mutate(country = case_when(location_name == "Global" ~ "Global", 
-                             .default = country)) |> 
-  filter(!is.na(country))
-
-# **Maternal haemorrhage** includes both postpartum haemorrhage (defined as blood loss ≥500 ml for vaginal delivery and ≥1000 ml for caesarean delivery) and antepartum haemorrhage (defined as vaginal bleeding from any cause at or beyond 20 weeks of gestation).  
-# 
-# **Maternal sepsis** is defined as a temperature <36°C or >38°C and clinical signs of shock (systolic blood pressure <90 mmHg and tachycardia >120 bpm). **Other maternal infections** are defined as any maternal infections excluding HIV, STI, or not related to pregnancy. 
-# 
-# **Maternal hypertensive disorders** include gestational hypertension (onset after 20 weeks gestation), pre-eclampsia, severe preeclampsia, and eclampsia, but exclude chronic hypertension (onset prior to pregnancy or prior to 20 weeks gestation) unless superimposed preeclampsia or eclampsia develop.
-# 
-# **Maternal obstructed labour and uterine rupture** aggregates obstructed labour (arrest in the first or second stage of active labour despite sufficient contractions), uterine rupture (non-surgical breakdown of uterine wall), and fistula (an abnormal opening between the vagina and the bladder or rectum following childbirth). 
-# 
-# **Abortion** is defined as elective or medically indicated termination of pregnancy at any gestational age. **Miscarriage** is defined as spontaneous loss of pregnancy before 24 weeks of gestation with complications requiring medical care.
-# 
-# **Ectopic pregnancy** is defined as pregnancy occurring outside of the uterus.
-# 
-# **Indirect maternal deaths** are due to existing diseases that are exacerbated by pregnancy. Examples include maternal infections and parasitic diseases complicating pregnancy, childbirth, and the puerperium, and diabetes in pregnancy, childbirth, and the puerperium. 
-# 
-# **Late maternal deaths** are deaths that occur six weeks to one year after the end of pregnancy, excluding incidental deaths.
-# 
-# **Maternal deaths aggravated by HIV/AIDS** are deaths occurring in HIV-positive women whose pregnancy has exacerbated their HIV/AIDS, leading to death.
-# 
-# **Other direct maternal disorders** encompasses a wide range of maternal disorders that do not map to other diseases in the GBD cause list, including other fatal or non-fatal complications occurring during pregnancy, childbirth, and the postpartum period. 
-# 
-# See also Cresswell et al. for more background on causes of maternal deaths:  
-#   Cresswell, Jenny A, Monica Alexander, Michael Y C Chong, Heather M Link, Marija Pejchinovska, Ursula Gazeley, Sahar M A Ahmed, et al. “Global and Regional Causes of Maternal Deaths 2009–20: A WHO Systematic Analysis.” The Lancet Global Health 13, no. 4 (April 2025): e626–34. <https://doi.org/10.1016/S2214-109X(24)00560-6>.
+# Remove non-indicator objects -----------------------
+rm(
+  country_codes, country_list, gho_dimensions, gho_indicators, region_codes, 
+  state_geo, theme_labels, UN_region_codes, WB_income_codes, 
+  world_abortion_laws
+  )
